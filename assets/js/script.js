5755c15ab2b4defe6e2a98027510a7649ddffbab
juanovt10 <>
2023-06-21 19:44:18 +0000

    Add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,6 +29,10 @@ function runGame(gameType) {
 
     if (gameType === 'addition') {
         displayAdditionQuestion(num1, num2);
+    } else if (gameType === 'multiply') { 
+        displayMultiplyQuestion(num1, num2);
+    } else if (gameType === 'subtract') { 
+        displaySubtractQuestion(num1, num2);
     } else {
         alert(`Unkown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -68,7 +72,11 @@ function calculateCorrectAnswer() {
     let operator = document.getElementById('operator').innerText;
 
     if (operator === '+') {
-        return [operand1 + operand2, 'addition']
+        return [operand1 + operand2, 'addition'];
+    } else if (operator === 'x') {
+        return [operand1 * operand2, 'multiply'];
+    } else if (operator === '-') {
+        return [operand1 - operand2, 'subtract'];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw  `Unimplemented operator ${operator}. Aborting`
@@ -101,12 +109,18 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = '+';
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
     
+    // Here we use the tenary if operand1 is larger than operand2, amek the content of operand1 operand1, if is smaller, use operand2. 
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = '-';
 }
 
-function displayMultiplyQuestion() {
-    
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = 'x';
 }
 
 function displayDivisionQuestion() {
